a1fac980cc0d2d94a83aa8baf5538d8f2c6790a9
Tijn Kersjes <tijn@divbyzero.nl>
2018-01-17 17:39:58 +0100

    Removed prop-types dependency

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 import { mount } from 'enzyme'
-import PropTypes from 'prop-types'
 
 // mount the component and pass the store to its context
-export const connectedMount = (component, store) => mount(component, { context: { store: store }, childContextTypes: { store: PropTypes.object }})
+export const connectedMount = (component, store) => mount(component, { context: { store: store }, childContextTypes: { store: () => null }})
