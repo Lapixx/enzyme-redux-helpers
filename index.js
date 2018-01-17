3bbe95928b97d665000b1fd697f58345a9737718
Tijn Kersjes <tijn@divbyzero.nl>
2018-01-17 17:37:29 +0100

    Added childContextTypes to wrapped component

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import { mount } from 'enzyme'
+import PropTypes from 'prop-types'
 
 // mount the component and pass the store to its context
-export const connectedMount = (component, store) => mount(component, { context: { store: store }})
+export const connectedMount = (component, store) => mount(component, { context: { store: store }, childContextTypes: { store: PropTypes.object }})
