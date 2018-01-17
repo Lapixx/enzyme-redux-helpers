c1a94e4cafc328cbee85de8dcf6d12b0821f2652
Tijn Kersjes <tijn@divbyzero.nl>
2018-01-17 17:32:01 +0100

    Basic connectedMount function

---
+++ index.js
@@ -0,0 +1,4 @@
+import { mount } from 'enzyme'
+
+// mount the component and pass the store to its context
+export const connectedMount = (component, store) => mount(component, { context: { store: store }})
